7f56d37551f9e018ef66e73c9153661657156329
Rupesh Ranjan <rupesh.ranjan@outlook.in>
2025-10-24 13:52:01 +0530

    forgot and update password router added

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -19,8 +19,8 @@ function isValidSkills(skills) {
     );
 }
 
-// --- Main validation ---
-async function validateUserData(user) {
+// --- SignUp Data validation ---
+async function validateUserSingUpData(user) {
     const errors = [];
 
     const {
@@ -88,4 +88,41 @@ async function validateUserData(user) {
     return errors;
 }
 
-export { validateUserData };
+// --- EditUser Data validation ---
+async function validateUserEditData(user) {
+    const errors = [];
+
+    const { firstName, lastName, age, gender, photoUrl, about, skills } = user;
+
+    if (firstName && !isNonEmptyString(firstName)) {
+        errors.push("First name is required and must be a non-empty string.");
+    }
+
+    if (lastName && !isNonEmptyString(lastName)) {
+        errors.push("Last name must be a non-empty string.");
+    }
+
+    if (typeof age !== "number" || age < 14) {
+        errors.push("Age must be a number and at least 14.");
+    }
+
+    if (!isValidGender(gender)) {
+        errors.push("Gender must be male, female, or other.");
+    }
+
+    if (photoUrl && !validator.isURL(photoUrl)) {
+        errors.push("Photo URL is not valid.");
+    }
+
+    if (about && typeof about !== "string" && about.length < 1000) {
+        errors.push("About must be a string.");
+    }
+
+    if (skills && !isValidSkills(skills)) {
+        errors.push("Skills must be an array of 1–50 non-empty strings.");
+    }
+
+    return errors;
+}
+
+export { validateUserSingUpData, validateUserEditData };
