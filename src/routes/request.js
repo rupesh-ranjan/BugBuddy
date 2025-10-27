52f2d3af076dd582ebf8511ac92e4a935a663d9c
Rupesh Ranjan <rupesh.ranjan@outlook.in>
2025-10-27 20:21:36 +0530

    fix for restricting sending request you own

--- src/routes/request.js
+++ src/routes/request.js
@@ -13,8 +13,7 @@ router.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
         const fromUserId = req.user._id;
         const toUserId = new Types.ObjectId(req.params.toUserId);
         const toUser = await User.findById(toUserId);
-        if (toUserId === fromUserId) {
-            console.log("Entereds");
+        if (toUserId.equals(fromUserId)) {
             throw new Error("Can't send request to yourself");
         }
         if (!toUser) throw new Error("No user found");
@@ -42,5 +41,19 @@ router.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
         res.status(400).send("ERROR: " + error.message);
     }
 });
+
+router.post(
+    "/request/review/:status/:requestId",
+    userAuth,
+    async (req, res) => {
+        // "accepted", "rejected"
+
+        const user = req.user;
+        console.log(user);
+        const request = await ConnectionRequest.findById(req.params.requestId);
+        console.log(request);
+        res.json({ message: `${req.user.firstName} Hi` });
+    }
+);
 
 export default router;
