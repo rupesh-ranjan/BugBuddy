829fbd643dfa0af7c098abdae1d34dbcb611a7c8
Rupesh Ranjan <rupesh.ranjan@outlook.in>
2025-11-02 18:25:13 +0530

    improved response when token is not found

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,7 +4,8 @@ import jwt from "jsonwebtoken";
 const userAuth = async (req, res, next) => {
     try {
         const { token } = req.cookies;
-        if (!token) throw new Error("Invalid Token");
+        if (!token)
+            return res.status(401).send("Access denied. No token provided.");
         const { _id } = jwt.verify(token, "mysecretkey");
 
         const user = await User.findById(_id);
